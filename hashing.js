71d3fed145a9d31b07c0e4b52bdc594b5613e38c
Sufiyan726 <139621412+Sufiyan726@users.noreply.github.com>
2023-09-30 15:49:24 +0530

    change

--- hashing.js
+++
@@ -1,8 +0,0 @@
-// server.js (Node.js example)
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-
-// During registration
-const password = req.body.password;
-const hashedPassword = await bcrypt.hash(password, saltRounds);
-// Store hashedPassword in the database
